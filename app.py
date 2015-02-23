be9b2dd9b96cb731d7c42db73c9d189ec9d8b0d4
Utsav2 <ukshah2@illinois.edu>
2015-02-23 13:23:44 -0600

    Initial

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,5 +7,5 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(debug=True, host='0.0.0.0', port=port)
